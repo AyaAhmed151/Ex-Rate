49a4d5f0b3b50d44767516b37e2db01117958bc6
AyaAhmed151 <88742241+AyaAhmed151@users.noreply.github.com>
2021-08-11 01:16:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+include "header.php";
+include "navbar.php";
+?>
+<div class="home-page">
+  <div class="homepage-content">
+    <div class="container">
+      <div class="text-content">
+        
+        <div class="sign">
+      <span class="fast-flicker">E</span>X-<span class="flicker">Rat</span>e
+    </div>
+    <br>
+        <p>
+        The exchange rate website provides the best services for new and used cars
+        and cars that you want to exchange, the website allows you to exchange
+        your car with a modern car and you will pay money,and you can also exchange
+        your car with a car older than your car and you will receive
+        money from the person who exchanged his car with you,
+        There is also a section for evaluating new and old cars 
+        by writing your evaluation on the car, and also you can request<br>
+        <br>
+        an evaluation from people on a specific car that you are adding,<br>
+        and there are also auctions on selling cars. You can create features or<br>
+        you can subscribe to win the car you want
+          
+        </p>
+        <a href="exchange.php ">
+          <button class="btn mt-2 btn-primary">
+            Find your car &nbsp;<i class="fas fa-caret-right"></i>
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php
+include "footer.php";
+
+?>
